bfcea6e97c5041bba0d211977f8a77c394bf9e4e
Lee Rowlands <lee.rowlands@previousnext.com.au>
2014-06-28 08:14:04 +1000

    [theme] Skeleton sparrow theme

---
+++ app/profiles/drupalinfo/themes/custom/sparrow/js/script.js
@@ -0,0 +1,15 @@
+/**
+ * @file
+ * A JavaScript file for the theme.
+ */
+
+(function ($, Drupal, window, document, undefined) {
+
+Drupal.behaviors.sparrow = {
+  attach: function(context, settings) {
+
+  }
+};
+
+
+})(jQuery, Drupal, this, this.document);
